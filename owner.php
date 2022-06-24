ac044c8510498473c197946583ff2ed8d894b7d9
Clarence Manuel <rencedm112@gmail.com>
2022-06-24 21:22:56 +0800

    Initial commit

--- owner.php
+++ owner.php
@@ -7,12 +7,25 @@
 
 <body>
 <form>
-  <label>Date<input type="datetime-local"/></label><button>search</button>
-  <label>Total Number of Customer<input type="number" readonly/></label>
-  <label>Initial Cash<input type="number" readonly/></label>
-  <label>Total Expense Today<input type="number" readonly/></label>
-  <label>Income Earned Today<input type="number" readonly/></label>
-  <label>Total Income Earned Today<input type="number" readonly/></label>
+  <label>Date
+    <input type="datetime-local" autofocus/>
+  </label>
+  <button>search</button>
+  <label>Total Number of Customer
+    <input type="number" value="0" readonly/>
+  </label>
+  <label>Initial Cash
+    <input type="number" value="0" readonly/>
+  </label>
+  <label>Total Expense Today
+    <input type="number" value="0" readonly/>
+  </label>
+  <label>Income Earned Today
+    <input type="number" value="0" readonly/>
+  </label>
+  <label>Total Income Earned Today
+    <input type="number" value="0" readonly/>
+  </label>
 </form>
 </body>
 </html>
