ee735f1f7942648ff47573d15994dfa7dd8ea3ec
Clarence Manuel <rencedm112@gmail.com>
2022-06-29 21:28:13 +0800

    Final v2

--- db/migrations/2022_06_28_14_31_17_129_129646_init/down.php
+++ db/migrations/2022_06_28_14_31_17_129_129646_init/down.php
@@ -1,8 +1,8 @@
 <?php
 $down_sql = <<< SQL
 DROP TABLE IF EXISTS services;
-DROP TABLE IF EXISTS service_for_customer;
-DROP TABLE IF EXISTS initial;
+DROP TABLE IF EXISTS service_for_customers;
+DROP TABLE IF EXISTS initials;
 SQL;
 
 return $down_sql;
